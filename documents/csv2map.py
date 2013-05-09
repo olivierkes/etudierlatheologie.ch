6ed5be63ad264c2a935d57d35c56737ab3864f4a
Nicolas Friedli <nicolas.friedli@gmail.com>
2013-05-09 17:16:57 +0200

    On ajoute l'icone catho. Et on garde le centrage à la main.

--- documents/csv2map.py
+++ documents/csv2map.py
@@ -12,7 +12,7 @@ template="""
 <div id="map" style="height: 400px;"></div>
 <script>
      //initialisation de la carte
-     var map = L.map('map').setView([46,7], 6);
+     var map = L.map('map').setView([48.070738,6.183471], 6);
      //Utilisation d'OSM
      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
      attribution: 'Map data © OpenStreetMap contributors',
@@ -27,30 +27,33 @@ template="""
     popupAnchor: [1, -34]
 });
 {% for d in data -%}
-var fac{{ loop.index }} = L.marker([{{ d.geo }}]).addTo(map); 
+var fac{{ loop.index }} = L.marker([{{ d.geo }}]{% if d.theologie == "Catholique" %}, {icon: catho}{% endif %}).addTo(map); 
 fac{{ loop.index }}.bindPopup('<b>{{ d.faculte|e }}</b><br />{% if d.theologie %}<i>{{ d.theologie|e }}</i><br />{% endif %}{{ d.universite|e }} - {{ d.ville|e }}<br /><a href="#">présentation</a> | <a href="{{ d.web }}" target="_blank">site web</a>');
 {% endfor -%}
 </script>
 """
 
-if __name__ == "__main__":
-    
-    # Si une base est donnée en argument, on la prend.
-    # Sinon on suppose que le script n'a pas bougé
-    if len(sys.argv) >= 2:
-        db = sys.argv[1]
-    else:
-        db = 'facultes.csv'
-    
+# Si une base est donnée en argument, on la prend.
+# Sinon on suppose que le script n'a pas bougé
+if len(sys.argv) >= 2:
+    db = sys.argv[1]
+else:
+    db = 'facultes.csv'
+
+# En essaie d'ouvrir le fichier choisi
+try:
     f = open(db, 'r')
 
+except IOError:
+    print("Fichier introuvable")
+
+else: 
     # On fait du CSV un dictionnaire
     data = csv.DictReader(f, delimiter="\t")
-
+    
     page = jinja2.Template(template)
     
     # On passe directement le dictionnaire courant à Jinja
     print(page.render(locals()))
     
-
     f.close()
